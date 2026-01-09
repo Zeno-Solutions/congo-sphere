f3832b353486a5ee5ad8c730ffe834b7bcc0a013
Shadow8021 <gregoyaga@gmail.com>
2026-01-09 14:28:26 +0100

    test api 2

--- app/api/utils/fuctionApi.ts
+++ app/api/utils/fuctionApi.ts
@@ -1,13 +1,13 @@
 import { API_ENDPOINTS,API_BASE_URL } from "../config/api";
 
 async function getAllEvents() {
-    const response = await fetch(`${API_BASE_URL}${API_ENDPOINTS.GET_ALL_EVENTS}`);
+    const response = await fetch(`${API_BASE_URL}${API_ENDPOINTS.events.list}`);
     const data = await response.json();
     return data;
 }
 
 async function getEventById(id: string) {
-    const response = await fetch(`${API_BASE_URL}${API_ENDPOINTS.GET_EVENT_BY_ID(id)}`);
+    const response = await fetch(`${API_BASE_URL}${API_ENDPOINTS.events.getById(id)}`);
     const data = await response.json();
     return data;
 }
