7396e9702607c5b6c2b5cb814edc95a9219683ea
Shadow8021 <gregoyaga@gmail.com>
2026-01-09 15:17:38 +0100

    test api 2

--- components/Recommended/Recommended.tsx
+++ components/Recommended/Recommended.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import  { useState } from "react";
 import { events } from "@/lib/data";
 import EventCard from "../ui/EventCard";
 import Contenaire from "../Contenaire";
