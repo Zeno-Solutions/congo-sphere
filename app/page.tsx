c7ccad02c3670052639c2473126c12bf01c414c0
Nick-Lemy <n.kayiranga@alustudent.com>
2026-01-27 22:51:15 +0200

    setup du project

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div />
+        <div className="flex w-full h-full items-center flex-col justify-center">
+          <Image
+            src={"/icon.jpg"}
+            alt={"Random name"}
+            width={100}
+            height={100}
+          />
+          <h1 className="text-3xl font-semibold">Comming Soon!</h1>
+        </div>
+        <div />
+      </main>
+    </div>
+  );
+}
