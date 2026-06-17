873d6a9bbd078ec0da71f575985b425487054662
Shadow8021 <gregoyaga@gmail.com>
2026-06-17 19:11:29 +0100

    resolution des erreurs tailwindcss

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -67,7 +67,7 @@ export default function NotFound() {
                 style={{ animationDelay: "0.2s" }}
               ></div>
               <div
-                className="w-2 h-2 bg-purple-600 rounded-full animate-bounce"
+                className="w-2 h-2 bg-purple-600 rounded-fulsl animate-bounce"
                 style={{ animationDelay: "0.4s" }}
               ></div>
             </div>
