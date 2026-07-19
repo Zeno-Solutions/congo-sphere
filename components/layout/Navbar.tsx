c32bc6e1f798f1b5a78590645e9ca7d119fbbb11
Shadow8021 <gregoyaga@gmail.com>
2026-07-19 10:15:38 +0100

    new nav button structure

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -62,15 +62,17 @@ export default function Navbar() {
             </div>
           )) || (
             <div className="flex items-center gap-4 px-10">
-              <Link href="/login">
-                <button className="text-slate-400 cursor-pointer font-bold font-label text-sm px-4 py-2 hover:text-white transition-all active:scale-95">
-                  Login
-                </button>
+              <Link
+                href="/login"
+                className="text-slate-400 cursor-pointer font-bold font-label rounded-full text-sm px-4 py-2 hover:text-white transition-all active:scale-95"
+              >
+                Login
               </Link>
-              <Link href="/register">
-                <button className="text-white text-on-primary-fixed cursor-pointer font-bold font-label text-sm px-6 py-2.5 rounded-full shadow-[0_0_20px_rgba(204,151,255,0.4)] hover:shadow-[0_0_30px_rgba(204,151,255,0.6)] transition-all active:scale-95">
-                  Register
-                </button>
+              <Link
+                href="/register"
+                className="text-white text-on-primary-fixed cursor-pointer font-bold font-label text-sm px-6 py-2.5 rounded-full shadow-[0_0_20px_rgba(204,151,255,0.4)] hover:shadow-[0_0_30px_rgba(204,151,255,0.6)] transition-all active:scale-95"
+              >
+                Register
               </Link>
             </div>
           )}
