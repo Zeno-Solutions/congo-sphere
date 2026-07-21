ffb48631557d0e166b40ce017510047e7f315b92
Shadow8021 <gregoyaga@gmail.com>
2026-07-22 00:43:30 +0100

    ticket state one

--- components/ui/ticket.tsx
+++ components/ui/ticket.tsx
@@ -56,12 +56,12 @@ export default function ticket() {
         </div>
       </div>
 
-      <div className="bg-[url('/bg1.jpeg')] bg-no-repeat  bg-cover border rounded-lg shadow-lg px-6 py-8 max-w-4xl mx-auto mt-8">
-        <h1 className="font-bold text-2xl my-4 text-center text-purple-400">
-          UPP COMERCE
+      <div className="bg-[url('/bg1.jpeg')] bg-no-repeat  bg-cover border rounded-lg shadow-lg px-6 py-4 max-w-4xl mx-auto mt-8">
+        <h1 className="font-bold text-2xl my-2 text-center text-white">
+          CONGO SHERE
         </h1>
         <hr className="mb-2" />
-        <div className="flex justify-between mb-6 ">
+        <div className="flex justify-between mb-1">
           <h1 className="text-lg font-bold">Invoice</h1>
           <div className="text-gray-700">
             <div>Date: 01/05/2023</div>
@@ -69,20 +69,32 @@ export default function ticket() {
           </div>
         </div>
 
-        <div className="flex justify-between transform skew-[20px]">
-          <div className="bg-yellow-200 my-5 max-w-100">
-            <h1 className="font-black ">
-              <span className="text-black text-3xl">Martial OYAGA</span>
-            </h1>
-            <div className="flex flex-col ">
-              <p className="text-gray-700 font-black">123 Main St.</p>
-              <p className="text-gray-700">Anytown, USA 12345</p>
-              <p className="text-gray-700">johndoe@example.com</p>
+        <div className="flex justify-between overflow-hidden max-h-50 bg-red-300 p-0">
+          <div className="flex ">
+            <div className="bg-yellow-200 z-20  min-w-100">
+              <h1 className="font-black ">
+                <span className="text-black text-3xl">Martial OYAGA</span>
+              </h1>
+              <div className="flex flex-col ">
+                <p className="text-gray-700 font-black">123 Main St.</p>
+                <p className="text-gray-700">Anytown, USA 12345</p>
+                <p className="text-gray-700">johndoe@example.com</p>
+              </div>
             </div>
+            <Image
+              src={"/loginTest.jpg"}
+              alt="dgd"
+              width={300}
+              height={130}
+              className="transform skew-9 h-100"
+            />
           </div>
+          <div className="bg-red-500 flex flex-col ">
+            <div className="h-1/2 flex items-center justify-center">
+              <span className="text-5xl font-black">VIP</span>
+            </div>
 
-          <div>
-            <Image src={"/qr.jpeg"} height={100} width={103} alt={"qr"} />
+            <Image src={"/qr.jpeg"} height={100} width={130} alt={"qr"} />
           </div>
         </div>
         <div className="text-gray-700 mb-2">Thank you for your business!</div>
