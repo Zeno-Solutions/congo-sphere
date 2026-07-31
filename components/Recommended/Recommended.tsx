6a45178a2d0125d9406a186ba5aa3ee206ec65a4
Shadow8021 <gregoyaga@gmail.com>
2026-07-31 19:16:25 +0100

    fin

--- components/Recommended/Recommended.tsx
+++ components/Recommended/Recommended.tsx
@@ -27,13 +27,13 @@ export default function Recommended() {
       : events.filter((event) => event.category === selectedCategory);
 
   return (
-    <Contenaire className="bg-yellow-400 w-full ">
+    <Contenaire className="w-full ">
       <section className="px-2 mx-auto max-w-400 flex flex-col justify-center">
-        <div className="flex flex-col md:flex-row md:items-center justify-between gap-4 mb-8 bg-amber-800">
+        <div className="flex flex-col md:flex-row md:items-center justify-between gap-4 mb-8 ">
           <h2 className="text-3xl font-bold font-headline text-white">
             Recommended For You
           </h2>
-          <div className="flex gap-3 overflow-x-auto no-scrollbar">
+          <div className="flex gap-3 overflow-x-auto no-scrollbar  rounded-3xl">
             {filterOptions.map((option) => (
               <button
                 key={option.value}
@@ -50,7 +50,7 @@ export default function Recommended() {
           </div>
         </div>
         {/* <!-- Bento Grid inspired layout --> */}
-        <div className="bg-red-600 min-w-70 mx-auto">
+        <div className="min-w-70 mx-auto">
           <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
             {filteredEvents.map((event) => (
               <EventCard key={event.id} event={event} />
