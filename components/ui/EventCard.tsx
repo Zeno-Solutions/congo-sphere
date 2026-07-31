f01b4f6c9e865f9c39031a5b94e776bdbe46c014
Shadow8021 <gregoyaga@gmail.com>
2026-07-31 18:08:27 +0100

    event card mobile

--- components/ui/EventCard.tsx
+++ components/ui/EventCard.tsx
@@ -19,7 +19,7 @@ interface EventCardProps {
   className?: string;
 }
 
-export default function EventCard({ event, className = "" }: EventCardProps) {
+export default function EventCard({ event }: EventCardProps) {
   const formatDate = (dateString: string) => {
     const date = new Date(dateString);
     return date.toLocaleDateString("en-US", {
@@ -35,7 +35,7 @@ export default function EventCard({ event, className = "" }: EventCardProps) {
       key={event.id}
       className="rounded-3xl min-w-80 lg:min-w-60 shadow-[-2px_12px_18px_-5px_#111d39] bg-surface-container-low  overflow-hidden flex flex-col group border border-white/5 hover:border-primary/30 transition-all"
     >
-      <div className="relative h-33 md:45 overflow-hidden">
+      <div className="relative h-45 overflow-hidden">
         <Image
           className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
           alt={event.title}
@@ -44,7 +44,7 @@ export default function EventCard({ event, className = "" }: EventCardProps) {
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
       </div>
-      <div className="text-white p-6 flex flex-col">
+      <div className="text-white p-4 flex flex-col">
         <div className="flex items-center justify-between text-[#cc97ff] gap-2 text-xs font-bold uppercase tracking-wider mb-3">
           <div className="flex flex-col">
             <span className="material-symbols-outlined text-[14px] ">
@@ -54,13 +54,15 @@ export default function EventCard({ event, className = "" }: EventCardProps) {
           </div>
           <div className="flex items-center gap-1">
             <MapPin size={16} />
-            <span className="max-w-30 text-[9px]">{event.location}</span>
+            <span className="max-w-30 text-left text-[9px]">
+              {event.location}
+            </span>
           </div>
         </div>
-        <h4 className="text-xl text-left line-clamp-2 overflow-hidden font-bold font-headline text-white mb-4 leading-tight">
+        <h4 className="text-lg md:text-xl text-left line-clamp-2 overflow-hidden font-bold font-headline text-white mb-4 leading-tight">
           {event.title}
         </h4>
-        <div className="flex items-center gap-2 text-on-surface-variant text-sm mb-4">
+        <div className="flex items-center gap-2 text-on-surface-variant text-sm mb-1">
           {event.tags.map((tag, index) => (
             <span
               key={index}
@@ -70,11 +72,11 @@ export default function EventCard({ event, className = "" }: EventCardProps) {
             </span>
           ))}
         </div>
-        <p className="text-on-surface-variant min-h-10 text-sm text-left font-body line-clamp-2 mb-6">
+        <p className="text-on-surface-variant min-h-10 text-sm text-left font-body line-clamp-2 mb-2">
           {event.description}
         </p>
-        <div className="mt-auto flex-1 flex justify-between px-3 items-center pt-4 border-t border-white/5 ">
-          <span className="text-xl font-bold text-white">
+        <div className="mt-auto flex-1 flex justify-between px-3 items-center pt-2 border-t border-white/5 ">
+          <span className="text-md md:text-xl font-bold text-white">
             {event.price ? (
               `$${event.price}`
             ) : (
