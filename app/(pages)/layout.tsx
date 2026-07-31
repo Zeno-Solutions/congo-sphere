b5e27c5809f7d4bf03a39fbb923235c7ecf79fb2
Shadow8021 <gregoyaga@gmail.com>
2026-07-31 17:27:03 +0100

    delete testnavbar

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from "react";
 import Navbar from "@/components/layout/Navbar";
-import Testnav from "@/components/layout/testnav";
 import Footer from "@/components/layout/Footer/Footer";
 import "../globals.css";
 export default function RootLayout({ children }: { children: ReactNode }) {
