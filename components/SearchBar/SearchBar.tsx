0ee42ffa0b95a38d54c965a5b7800dcc004cfa24
Shadow8021 <gregoyaga@gmail.com>
2026-07-31 11:43:18 +0100

    search bar fixed

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -17,7 +17,7 @@ export default function SearchBar({
 }: SearchBarProps) {
   return (
     <div
-      className={`mb-50 z-10 w-full absolute  top-15 md:top-30 mx-auto px-20 left-1 ${className || ""}`}
+      className={`mb-50 z-10 w-full bg-red-500 absolute flex justify-center  top-15 md:top-30 mx-auto px-20 left-1 ${className || ""}`}
     >
       <div className="relative border-none md:max-w-2xl  mx-auto">
         <span className="material-symbols-outlined z-10 absolute left-5 top-1/2 transform -translate-y-1/2 ">
@@ -25,7 +25,7 @@ export default function SearchBar({
         </span>
 
         <input
-          className="w-full rounded-full pl-12 h-14 border border-slate-700 bg-slate-950/80 px-4 py-3 text-white outline-none focus:border-purple-400 focus:ring-2 focus:ring-purple-500/20"
+          className="w-full min-w-85 rounded-full pl-12 h-14 border border-slate-700 bg-slate-950/80 px-4 py-3 text-white outline-none focus:border-purple-400 focus:ring-2 focus:ring-purple-500/20"
           placeholder={placeholder}
           type="text"
           name={name}
